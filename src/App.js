8922f30df839e279f4483feb0570cfe03c1d9567
nibchee <nirbhaybansal28@gmail.com>
2023-06-11 16:34:39 +0530

    Adding React Table Component & using Nested row component inside it

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import './App.css';
-import ToDoRowItem from './components/ToDoRowItem';
+import TodoTable from './components/ToDoTable';
 
 function App() {
+  //array of objects
   const todos=[
     {rowNumber:1,rowDescription:'Feed puppy',rowAssigned:'User One'},
     {rowNumber:2,rowDescription:'Water plants',rowAssigned:'User Two'},
     {rowNumber:3,rowDescription:'Make Dinner',rowAssigned:'User Three'}]
+
   return (
     <div className="mt-5 container">
       <div className="card">
         <div className="card-header">Your Todos</div>
         <div className="card-body">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Description</th>
-                <th scope="col">Assigned</th>
-              </tr>
-            </thead>
-            <tbody>
-              <ToDoRowItem 
-              rowNumber={todos[0].rowNumber} 
-              rowDescription={todos[0].rowDescription} 
-              rowAssigned={todos[0].rowAssigned}/>
-
-              <ToDoRowItem 
-              rowNumber={todos[1].rowNumber} 
-              rowDescription={todos[1].rowDescription} 
-              rowAssigned={todos[1].rowAssigned}/>
-
-              <ToDoRowItem 
-              rowNumber={todos[2].rowNumber} 
-              rowDescription={todos[2].rowDescription} 
-              rowAssigned={todos[2].rowAssigned}/>
-              
-            </tbody>
-          </table>
+         <TodoTable todos={todos}/>
         </div>
       </div>
     </div>
