7cae697e132a8aa2ee8a39912b64b09eedda01a1
nibchee <nirbhaybansal28@gmail.com>
2023-06-11 18:05:11 +0530

    Adding Click Listeners

--- src/App.js
+++ src/App.js
@@ -3,19 +3,35 @@ import TodoTable from './components/ToDoTable';
 
 function App() {
   //array of objects
-  const todos=[
-    {rowNumber:1,rowDescription:'Feed puppy',rowAssigned:'User One'},
-    {rowNumber:2,rowDescription:'Water plants',rowAssigned:'User Two'},
-    {rowNumber:3,rowDescription:'Make Dinner',rowAssigned:'User Three'},
-    {rowNumber:4,rowDescription:'Charge Phone Battery',rowAssigned:'User Four'}
+  const todos = [
+    { rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One' },
+    { rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two' },
+    { rowNumber: 3, rowDescription: 'Make Dinner', rowAssigned: 'User Three' },
+    { rowNumber: 4, rowDescription: 'Charge Phone Battery', rowAssigned: 'User Four' }
   ]
+
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: 'New Todo',
+        rowAssigned: 'user Three'
+      }
+      todos.push(newTodo);
+      console.log(todos);
+    }
+    console.log('Our add Todo btn has been clicked')
+  }
 
   return (
     <div className="mt-5 container">
       <div className="card">
         <div className="card-header">Your Todos</div>
         <div className="card-body">
-         <TodoTable todos={todos}/>
+          <TodoTable todos={todos} />
+          <button className='btn btn-primary' onClick={addTodo}>
+            Add new todo
+          </button>
         </div>
       </div>
     </div>
--- src/components/ToDoRowItem.js
+++ src/components/ToDoRowItem.js
@@ -1,12 +1,12 @@
-function ToDoRowItem(props){
-   
-    return(
-     <tr>
-        <th scope="row">{props.rowNumber}</th>
-        <td>{props.rowDescription}</td>
-        <td>{props.rowAssigned}</td>
-     </tr>
-     )
-} 
+function ToDoRowItem(props) {
+
+    return (
+        <tr>
+            <th scope="row">{props.rowNumber}</th>
+            <td>{props.rowDescription}</td>
+            <td>{props.rowAssigned}</td>
+        </tr>
+    )
+}
 
 export default ToDoRowItem
