888b2162203447f19f7aea084fc03b5f6829afb2
nibchee <nirbhaybansal28@gmail.com>
2023-06-11 18:58:23 +0530

    Todo Form created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './App.css';
 import TodoTable from './components/ToDoTable';
+import NewTodoForm from './components/NewTodoForm';
 
 function App() {
   //array of objects
@@ -45,6 +46,7 @@ function App() {
           <button className='btn btn-primary' onClick={addTodo}>
             Add new todo
           </button>
+          <NewTodoForm />
         </div>
       </div>
     </div>
